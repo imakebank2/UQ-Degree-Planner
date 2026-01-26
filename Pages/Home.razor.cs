9b4a4f2a35b5aef73208b3897bbb8fb6fc8bfe2b
💤 <willzg12345@outlook.com>
2026-01-26 19:09:06 +1000

    I guess i'm continuing

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,16 @@
+namespace UQDegreePlanner.Pages;
+public partial class Home
+{
+    public static class Limits
+    {
+        public const int MinSemesters = 1;
+        public const int MaxSemesters = 20;
+        public const int MaxCharacters = 50;
+        public const int MinCoursesPerSemester = 1;
+        public const int MaxCoursesPerSemester = 10;
+        public const int MaxNumericFieldWidthInPixels = 200;
+        public const int MaxCharacterLength = 100;
+    }
+
+    
+}
